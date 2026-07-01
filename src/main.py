7bbef30bb2b22e39c0b49e2290326c45200daa68
Vishalb2002 <Vishalbadhan2002@gmail.com>
2026-07-01 14:01:15 +0530

    check

--- src/main.py
+++ src/main.py
@@ -6,45 +6,45 @@ from retrieval.retriever import Retriever
 
 def main():
 
-    # loader = PDFLoader(
-    #     input_dir="data/raw/hand_gestures",
-    #     output_dir="data/processed/hand_gestures"
-    # )
+    loader = PDFLoader(
+        input_dir="data/raw/hand_gestures",
+        output_dir="data/processed/hand_gestures"
+    )
 
-    # loader.process_all_pdfs()
+    loader.process_all_pdfs()
 
-      # -----------------------------
+    #   -----------------------------
     # Step 2 : Chunk Text
     # -----------------------------
-    # chunker = TextChunker(
-    #     input_dir="data/processed/hand_gestures",
-    #     output_dir="data/chunks/hand_gestures"
-    # )
+    chunker = TextChunker(
+        input_dir="data/processed/hand_gestures",
+        output_dir="data/chunks/hand_gestures"
+    )
 
-    # chunker.process_all_files()
+    chunker.process_all_files()
 
-     # STEP 3
-    # embedder = SBERTEmbedder(
-    #     input_dir="data/chunks/hand_gestures"
-    # )
+    #  STEP 3
+    embedder = SBERTEmbedder(
+        input_dir="data/chunks/hand_gestures"
+    )
 
-    # embedded_chunks = embedder.process_all_files()
+    embedded_chunks = embedder.process_all_files()
 
-    # print(embedded_chunks[5])
+    print(embedded_chunks[5])
 
-    #  # --------------------------
-    # # STEP 4
-    # # --------------------------
+     # --------------------------
+    # STEP 4
+    # --------------------------
 
-    # vector_store = VectorStore()
+    vector_store = VectorStore()
 
-    # vector_store.add_embeddings(
-    #     embedded_chunks
-    # )
+    vector_store.add_embeddings(
+        embedded_chunks
+    )
 
-    # print(
-    #     f"\nTotal Chunks in DB : {vector_store.total_chunks()}"
-    # )
+    print(
+        f"\nTotal Chunks in DB : {vector_store.total_chunks()}"
+    )
 
 
     retriever = Retriever()
